9ff1f86a9bba72c71b7e9e01b9e3809a47600908
David Sudia <dsudia@fanatics.com>
2017-05-31 11:35:06 -0600

    make this.copy this.fs.copy

--- generators/server/index.js
+++ generators/server/index.js
@@ -25,63 +25,63 @@ module.exports = class extends Generator {
     writing() {
         const name = this.config.get('name')
         // copy ssl
-        this.copy('certs/test/Makefile', './certs/test/Makefile')
+        this.fs.copy('certs/test/Makefile', './certs/test/Makefile')
         // copy protocol
-        this.copy('protocol/hello.pb.go', './protocol/hello.pb.go')
-        this.copy('protocol/hello.pb.gw.go', './protocol/hello.pb.gw.go')
-        this.copy('protocol/hello.proto', './protocol/hello.proto')
+        this.fs.copy('protocol/hello.pb.go', './protocol/hello.pb.go')
+        this.fs.copy('protocol/hello.pb.gw.go', './protocol/hello.pb.gw.go')
+        this.fs.copy('protocol/hello.proto', './protocol/hello.proto')
         // copy server
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('server/rpc.defs.go'),
             this.destinationPath('./server/rpc.defs.go'),
             { appname: name }
         ),
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('server/serve_test.go'),
             this.destinationPath('./server/serve_test.go'),
             { appname: name }
         ),
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('server/serve.go'),
             this.destinationPath('./server/serve.go'),
             { appname: name }
         ),
         // copy swagger
-        this.copy('swagger/favicon-16x16.png', './swagger/favicon-16x16.png'),
-        this.copy('swagger/favicon-32x32.png', './swagger/favicon-32x32.png'),
-        this.copy('swagger/index.html', './swagger/index.html'),
-        this.copy('swagger/oauth2-redirect.html', './swagger/oauth2-redirect.html'),
-        this.copy('swagger/protocol.swagger.json', './swagger/protocol.swagger.json'),
-        this.copy('swagger/swagger-ui-bundle.js', './swagger/swagger-ui-bundle.js'),
-        this.copy('swagger/swagger-ui-bundle.js.map', './swagger/swagger-ui-bundle.js.map'),
-        this.copy('swagger/swagger-ui-standalone-preset.js', './swagger/swagger-ui-standalone-preset.js'),
-        this.copy('swagger/swagger-ui-standalone-preset.js.map', './swagger/swagger-ui-standalone-preset.js.map'),
-        this.copy('swagger/swagger-ui.css', './swagger/swagger-ui.css'),
-        this.copy('swagger/swagger-ui.css.map', './swagger/swagger-ui.css.map'),
-        this.copy('swagger/swagger-ui.js', './swagger/swagger-ui.js'),
-        this.copy('swagger/swagger-ui.js.map', './swagger/swagger-ui.js.map'),
+        this.fs.copy('swagger/favicon-16x16.png', './swagger/favicon-16x16.png'),
+        this.fs.copy('swagger/favicon-32x32.png', './swagger/favicon-32x32.png'),
+        this.fs.copy('swagger/index.html', './swagger/index.html'),
+        this.fs.copy('swagger/oauth2-redirect.html', './swagger/oauth2-redirect.html'),
+        this.fs.copy('swagger/protocol.swagger.json', './swagger/protocol.swagger.json'),
+        this.fs.copy('swagger/swagger-ui-bundle.js', './swagger/swagger-ui-bundle.js'),
+        this.fs.copy('swagger/swagger-ui-bundle.js.map', './swagger/swagger-ui-bundle.js.map'),
+        this.fs.copy('swagger/swagger-ui-standalone-preset.js', './swagger/swagger-ui-standalone-preset.js'),
+        this.fs.copy('swagger/swagger-ui-standalone-preset.js.map', './swagger/swagger-ui-standalone-preset.js.map'),
+        this.fs.copy('swagger/swagger-ui.css', './swagger/swagger-ui.css'),
+        this.fs.copy('swagger/swagger-ui.css.map', './swagger/swagger-ui.css.map'),
+        this.fs.copy('swagger/swagger-ui.js', './swagger/swagger-ui.js'),
+        this.fs.copy('swagger/swagger-ui.js.map', './swagger/swagger-ui.js.map'),
         // copy vendor
-        this.copy('vendor/vendor.json', './vendor/vendor.json'),
+        this.fs.copy('vendor/vendor.json', './vendor/vendor.json'),
         // copy main dir
-        this.copy('.gitignore', './.gitignore'),
-        this.copy('docker-test.sh', './docker-test.sh'),
-        this.copy('version', './version'),
-         this.copyTpl(
+        this.fs.copy('.gitignore', './.gitignore'),
+        this.fs.copy('docker-test.sh', './docker-test.sh'),
+        this.fs.copy('version', './version'),
+         this.fs.copyTpl(
             this.templatePath('README.md'),
             this.destinationPath('./README.md'),
             { appname: name }
         ),
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('circle.yml'),
             this.destinationPath('./circle.yml'),
             { appname: name }
         ),
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('Dockerfile'),
             this.destinationPath('./Dockerfile'),
             { appname: name }
         ),
-        this.copyTpl(
+        this.fs.copyTpl(
             this.templatePath('main.go'),
             this.destinationPath('./main.go'),
             { appname: name }
